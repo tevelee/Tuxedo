8464851abdf32f3af3b33c660f823f24ad552630
Laszlo Teveli <laszlo.teveli@skyscanner.net>
2018-08-13 22:02:03 +0200

    Updating Eval

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["Tuxedo"])
     ],
     dependencies: [
-        .package(url: "https://github.com/tevelee/Eval", from: "1.3.2")
+        .package(url: "https://github.com/tevelee/Eval", from: "1.3.3")
     ],
     targets: [
         .target(
